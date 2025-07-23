fe8992aa2f728bbce85b8ad8527ed06f369b0841
Сергей <sergowindow@yandex.ru>
2025-07-23 19:53:40 +0300

    initial commit

---
+++ src/test/java/org/skypro/question_generator/QuestionGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.question_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
